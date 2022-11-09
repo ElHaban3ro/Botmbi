f58b887de13a3a0208dc45c27e9994ac7a8f491b
ElHaban3ro <Ferdhaban@gmail.com>
2022-11-09 14:57:55 -0800

    (v1.4) Se añaden las configuraciones al archivo de comunidades!

--- src/app.py
+++ src/app.py
@@ -40,10 +40,10 @@ async def get_prefix_custom(bot, message):
     global prefix_see
     guild = message.guild
 
-    if str(guild.id) in list(prefix_local_dict.keys()):
-        
+    if str(guild.id) in list(prefix_local_dict.keys()):        
         prefix_see = prefix_local_dict[str(guild.id)]['prefix']
         return prefix_local_dict[str(guild.id)]['prefix']
+
 
     else:
         prefix_see = prefix_local_dict['General']['prefix']
@@ -79,14 +79,15 @@ async def prefix(ctx, *args):
                 
 
                 if str(ctx.guild.id) in list(communities_json.keys()):
-                    communities_json.pop(str(ctx.guild.id))
+                    communities_json[str(ctx.guild.id)]['prefix'] = new_prefix
 
-
-                communities_json.update({str(ctx.guild.id): {'communityName': ctx.guild.name, 'prefix': new_prefix}})
+                else:
+                    communities_json[str(ctx.guild.id)] = {'communityName': ctx.guild.name, 'prefix': new_prefix}
+                
 
 
                 global prefix_local_dict            
-                prefix_local_dict['SEXO'] = 'sí?'
+                prefix_local_dict['SEXO'] = 'sí?' # xd?
                 prefix_local_dict = communities_json
                     
 
@@ -142,13 +143,78 @@ async def config(ctx, *args):
             if config_app == 'ombi': # Si se quiere configurar ombi!
                 if config_name == 'host':
                     if 'https://' in config_value or 'http://' in config_value:
-                        await ctx.channel.send('*Valid Host*')
-                        community_data = {'communityId': ctx.guild.id, 'communityName': ctx.guild.name, 'prefix': get_prefix_custom(str(ctx.author.id))}
 
-                        print(community_data)
+                        with open('./src/communities.json', 'r+') as f_u:
+                            read_f_u = json.load(f_u)
+
+                            if str(ctx.guild.id) in list(read_f_u.keys()): # Si está ya en la lista, solo puede significar 2 cosas: o ya se configuró antes, o se estableció antes el prefixs
+
+                                read_f_u[str(ctx.guild.id)]['ombiHost'] = config_value
+
+
+                            else:
+                                read_f_u[str(ctx.guild.id)] = {
+                                    'communityName': ctx.guild.name,
+                                    'ombiHost': config_value,
+                                    'prefix': read_f_u['General']['prefix']
+                                }
+
+
+                            
+
+                            f_u.seek(0)
+                            json.dump(read_f_u, f_u, indent = 4)
+                            f_u.truncate()
+
+
+                            embed = discord.Embed(title = 'BOTMBI CONFIGURED!', description=f"Your Botmbi's custom host was configured correctly as: ***{config_value}***", color=0xFFD062)
+                            embed.set_author(name = f'Hi, {str(ctx.author)[:str(ctx.author).find("#")]}', url = 'https://github.com/elhaban3ro', icon_url = ctx.author.avatar)
+                            embed.set_footer(text = f'view more with {prefix_see}help', icon_url='https://www.pngmart.com/files/12/Twitter-Verified-Badge-PNG-HD.png')
+
+                            await ctx.reply(embed = embed)
 
                     else:
-                        await ctx.reply('*Invalid value :(*')
+                        embed = discord.Embed(title = 'Ombi host incorrect | BOTMBI', description=f'The provided **host** of your Ombi is incorrect. Remember to add the http method. Enter the command as follows: ```{prefix_see}config ombi host http://myombi.io/```', color=0xFFD062)
+                        embed.set_author(name = f'Hi, {str(ctx.author)[:str(ctx.author).find("#")]}', url = 'https://github.com/elhaban3ro', icon_url = ctx.author.avatar)
+                        embed.set_footer(text = f'view more with {prefix_see}help', icon_url='https://www.pngmart.com/files/12/Twitter-Verified-Badge-PNG-HD.png')
+
+                        await ctx.reply(embed = embed)
+
+
+
+
+
+                elif config_name == 'apikey':
+                    with open('./src/communities.json', 'r+') as json_file:
+                        read_f = json.load(json_file)
+
+                        if str(ctx.guild.id) in list(read_f.keys()):
+                            if 'prefix' not in list(read_f[str(ctx.guild.id)]):
+                                read_f[str(ctx.guild.id)]['prefix'] = read_f['General']['prefix']
+
+
+                            read_f[str(ctx.guild.id)]['ombiApikey'] = config_value
+
+                        else:
+                            read_f[str(ctx.guild.id)] = {'communityName': ctx.guild.name, 'prefix': read_f['General']['prefix']}
+
+
+
+
+                        json_file.seek(0)
+                        json.dump(read_f, json_file, indent = 4)
+                        json_file.truncate()
+                
+                    embed = discord.Embed(title = 'BOTMBI CONFIGURED!', description=f"Your Botmbi's custom **apikey** was configured correctly as: ***{config_value}***", color=0xFFD062)
+                    embed.set_author(name = f'Hi, {str(ctx.author)[:str(ctx.author).find("#")]}', url = 'https://github.com/elhaban3ro', icon_url = ctx.author.avatar)
+                    embed.set_footer(text = f'view more with {prefix_see}help', icon_url='https://www.pngmart.com/files/12/Twitter-Verified-Badge-PNG-HD.png')
+
+                    await ctx.reply(embed = embed)
+
+
+
+
+
 
                     
 
@@ -158,7 +224,7 @@ async def config(ctx, *args):
 
 
                 else:
-                    embed = discord.Embed(title = 'BOTMBI', description='Option not available\nFollow the format below:\n\t```!config [ombi] [host, apikey] [value]```', url='https://github.com/elhaban3ro', color=0xFFD062)
+                    embed = discord.Embed(title = 'BOTMBI', description=f'Option not available\nFollow the format below:\n\t```{prefix_see}config [ombi] [host, apikey] [value]```', url='https://github.com/elhaban3ro', color=0xFFD062)
 
                     embed.set_footer(text = f'view more with {prefix_see}help', icon_url='https://www.pngmart.com/files/12/Twitter-Verified-Badge-PNG-HD.png')
                     embed.set_author(name = f'Hi, {str(ctx.author)[:str(ctx.author).find("#")]}', url = 'https://github.com/elhaban3ro', icon_url = ctx.author.avatar)
@@ -167,7 +233,7 @@ async def config(ctx, *args):
 
 
             else:
-                embed = discord.Embed(title = 'BOTMBI', description='Option not available\nFollow the format below:\n\t```!config [ombi] [host, apikey] [value]```', url='https://github.com/elhaban3ro', color=0xFFD062)
+                embed = discord.Embed(title = 'BOTMBI', description=f'Option not available\nFollow the format below:\n\t```{prefix_see}config [ombi] [host, apikey] [value]```', url='https://github.com/elhaban3ro', color=0xFFD062)
 
                 embed.set_footer(text = f'view more with {prefix_see}help', icon_url='https://www.pngmart.com/files/12/Twitter-Verified-Badge-PNG-HD.png')
                 embed.set_author(name = f'Hi, {str(ctx.author)[:str(ctx.author).find("#")]}', url = 'https://github.com/elhaban3ro', icon_url = ctx.author.avatar)
@@ -176,7 +242,7 @@ async def config(ctx, *args):
 
 
         else:
-            embed = discord.Embed(title = 'BOTMBI', description='Be sure to pass the appropriate parameters.\nFollow the format below:\n\t```!config [ombi] [host, apikey] [value]```', url='https://github.com/elhaban3ro', color=0xFFD062)
+            embed = discord.Embed(title = 'BOTMBI', description=f'Be sure to pass the appropriate parameters.\nFollow the format below:\n\t```{prefix_see}config [ombi] [host, apikey] [value]```', url='https://github.com/elhaban3ro', color=0xFFD062)
             embed.set_footer(text = f'view more with {prefix_see}help', icon_url='https://www.pngmart.com/files/12/Twitter-Verified-Badge-PNG-HD.png')
             embed.set_author(name = f'Hi, {str(ctx.author)[:str(ctx.author).find("#")]}', url = 'https://github.com/elhaban3ro', icon_url = ctx.author.avatar)
 
